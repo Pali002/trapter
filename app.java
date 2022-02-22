585128e15c2d29e142431921d2805d1899443492
pali002 <zentaipali@gmail.com>
2022-02-22 09:10:27 +0100

    bekeres

--- app.java
+++ app.java
@@ -15,4 +15,15 @@ class app{
 		System.out.println("Zentai Pál, 2022-02-22");
 		System.out.println("Feladat 0316, trapéz területszámítás");
 
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("aOldal: ");
+		int aOldal = sc.nextInt();
+		System.out.print("bOldal: ");
+		int bOldal = sc.nextInt();
+		System.out.print("cOldal: ");
+		int cOldal = sc.nextInt();
+		System.out.print("dOldal: ");
+		int dOldal = sc.nextInt();
+		
+		
